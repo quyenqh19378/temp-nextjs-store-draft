04a5dd482a8cae61f33017622e387284ded852a6
quyenhoang140299 <hoangquyen140299@gmail.com>
2024-07-26 11:38:56 +0700

    add auth provider

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,4 +1,23 @@
-function UserIcon() {
-    return <div>UserIcon</div>;
+/* eslint-disable @next/next/no-img-element */
+import { currentUser } from "@clerk/nextjs/server";
+import { LuUser2 } from "react-icons/lu";
+
+async function UserIcon() {
+    const user = await currentUser();
+    const profileImage = user?.imageUrl;
+
+    if (profileImage) {
+        return (
+            <img
+                src={profileImage}
+                alt='user image'
+                className='w-6 h-6 rounded-full object-cover'
+            />
+        );
+    }
+
+    return (
+        <LuUser2 className='w-6 h-6 bg-primary rounded-full text-white object-cover' />
+    );
 }
 export default UserIcon;
