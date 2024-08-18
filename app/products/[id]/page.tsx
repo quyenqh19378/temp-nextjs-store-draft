5e48e420f26b914571aa2c3743117625115ec12a
quyenhoang140299 <hoangquyen140299@gmail.com>
2024-08-18 22:06:29 +0700

    test

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,10 +1,10 @@
-import { fetchSingleProduct, findExistingReview } from "@/utils/actions";
-import { formatCurrency } from "@/utils/format";
 import BreadCrumbs from "@/components/single-product/BreadCrumbs";
+import { fetchSingleProduct, findExistingReview } from "@/utils/actions";
 import Image from "next/image";
+import { formatCurrency } from "@/utils/format";
 import FavoriteToggleButton from "@/components/products/FavoriteToggleButton";
-// import ProductRating from "@/components/single-product/ProductRating";
 import AddToCart from "@/components/single-product/AddToCart";
+import ProductRating from "@/components/single-product/ProductRating";
 import ShareButton from "@/components/single-product/ShareButton";
 import SubmitReview from "@/components/reviews/SubmitReview";
 import ProductReviews from "@/components/reviews/ProductReviews";
@@ -12,12 +12,11 @@ import { auth } from "@clerk/nextjs/server";
 
 async function SingleProductPage({ params }: { params: { id: string } }) {
     const product = await fetchSingleProduct(params.id);
-    const { name, price, company, image, description } = product;
+    const { name, image, company, description, price } = product;
     const dollarsAmount = formatCurrency(price);
     const { userId } = auth();
     const reviewDoesNotExist =
         userId && !(await findExistingReview(userId, product.id));
-
     return (
         <section>
             <BreadCrumbs name={product.name} />
@@ -28,36 +27,39 @@ async function SingleProductPage({ params }: { params: { id: string } }) {
                         src={image}
                         alt={name}
                         fill
-                        sizes='(max-width: 768px) 100vw, (max-width: 1200px) 75vw, 33vw'
+                        sizes='(max-width:768px) 100vw,(max-width:1200px) 50vw, 33vw'
                         priority
-                        className='w-full rounded-md object-cover'
+                        className='w-full rounded object-cover'
                     />
                 </div>
-
                 {/* PRODUCT INFO SECOND COL */}
                 <div>
                     <div className='flex gap-x-8 items-center'>
-                        <h1 className='capitalize text-3xl fond-bold'>
-                            {name}
+                        <h1 className='capitalize text-3xl font-bold'>
+                            {name}{" "}
                         </h1>
-                        {/* <div className='flex  items-center gap-x-2'>
+                        <div className='flex items-center gap-x-2'>
                             <FavoriteToggleButton productId={params.id} />
-                            <ShareButton productId={product.id} name={name} />
-                        </div> */}
+                            <ShareButton
+                                name={product.name}
+                                productId={params.id}
+                            />
+                        </div>
                     </div>
-                    {/* <ProductRating productId={params.id} /> */}
+                    <ProductRating productId={params.id} />
                     <h4 className='text-xl mt-2'>{company}</h4>
-                    <p className='mt-3 text-md bg-muted inline-block p-2 rounded-md'>
+                    <p className='mt-3 text-md bg-muted inline-block p-2 rounded'>
                         {dollarsAmount}
                     </p>
                     <p className='mt-6 leading-8 text-muted-foreground'>
                         {description}
                     </p>
-                    <AddToCart productId={product.id} />
+                    <AddToCart productId={params.id} />
                 </div>
             </div>
             <ProductReviews productId={params.id} />
-            <>{reviewDoesNotExist && <SubmitReview productId={params.id} />}</>
+
+            {reviewDoesNotExist && <SubmitReview productId={params.id} />}
         </section>
     );
 }
